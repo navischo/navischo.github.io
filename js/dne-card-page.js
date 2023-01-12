1f8babf1ccae156134139a8b002700827cb12646
beast1 <beast.loby@gmail.com>
2023-01-12 19:45:51 +0200

    finish first five quest cards

--- js/dne-card-page.js
+++ js/dne-card-page.js
@@ -8,13 +8,14 @@ const drawDoorCardPage = (cardData) => {
     } else {
         newCard.querySelector(".js.card-page__budget").style.display = "none";
     }
+    newCard.querySelector(".js.card-page__id").textContent = `#${cardData.id}`;
     newCard.querySelector(".js.card-page__profit").textContent = `${cardData.profit}`;
     newCard.querySelector(".js.card-page__level").textContent = `${cardData.level}lvl`;
     newCard.querySelector(".js.card-page__roles").textContent = `${cardData.roles}.`;
     newCard.querySelector(".js.card-page__origins").textContent = `${cardData.profitOrigins}.`;
     newCard.querySelector(".js.card-page__client").textContent = `Замовлення від ${cardData.client}`;
-    newCard.querySelector(".js.card-page__description").textContent = `${cardData.description}`;
+    newCard.querySelector(".js.card-page__description").innerHTML = `${cardData.description}`;
     newCard.querySelector(".js.card-page__penalty").textContent = `${cardData.penalty}`;
-    newCard.querySelector(".js.card-page__bonus").textContent = `${cardData.level}`;
+    // newCard.querySelector(".js.card-page__bonus").textContent = `${cardData.level}`;
     console.log('cardData', cardData);
 }
