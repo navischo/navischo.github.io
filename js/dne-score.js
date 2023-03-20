d930106bda8320c07d5aec9a3e34d5a8361824db
beast1 <beast.loby@gmail.com>
2023-03-20 22:09:56 +0200

    upd

--- js/dne-score.js
+++ js/dne-score.js
@@ -3,7 +3,10 @@ const needScoreNode = document.querySelector("#need-score");
 
 // todo добавить бонус от класса и инвентаря
 const initScore = () => {
-	currentPlayer.score = 0;
+	let crewPoints = 5; //  наемнички
+	let soundPoints = 18+3+3+6+15; //  саунд
+	let lootPoints = 4+9; // аппаратура
+	currentPlayer.score = crewPoints + soundPoints + lootPoints;
 // 	console.log("currentPlayer.classArr", currentPlayer.classArr);
 // 	currentPlayer.classArr.forEach((card) => {
 // 		console.log("card", card);
@@ -15,5 +18,15 @@ const initScore = () => {
 const updScore = (bonus = 0) => {
 	currentPlayer.score = currentPlayer.score + +bonus;
 	playerScoreNode.innerHTML = currentPlayer.score;
-	needScoreNode.innerHTML = 12;
+	needScoreNode.innerHTML = 36;
 }
+
+const compareScore = () => {
+	if (currentPlayer.score > needScoreNode.innerHTML) {
+		const LEVEL_COUNT = 2;
+		const LOOT_COUNT = 15;
+		const YOU_WIN = `You made it. Get ${LOOT_COUNT} treasure and ${LEVEL_COUNT} levels!`
+		console.log(YOU_WIN);
+	}
+}
+
