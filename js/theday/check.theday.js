c39e0b7e6d8db5722aa969dae4f14b06d8474058
Егор <beast.loby@gmail.com>
2023-05-25 09:11:57 +0300

    add standalone score page with hardcode

---
+++ js/theday/check.theday.js
@@ -0,0 +1,92 @@
+const checkData = {
+    budget: 36800,
+    income: 154000,
+    flex: 1000,
+    mob: 3270,
+    crew: 5000,
+    timeBonus: 13490,
+    special: 12000,
+    total: 222160
+};
+
+const drawCheck = (checkData, parent = "body") => {
+    const getCheckLayer = (data, wrapIndex) => {
+        const wrap = document.createElement("div");
+        const keys = Object.keys(data);
+        console.log("keys", keys);
+
+
+        let keysMarkup = "";
+        const pointsArr = [];
+        keys.forEach((key, strokeIndex) => {
+            pointsArr.push(data[key]);
+            keysMarkup = keysMarkup + `<p class="check-clone__text-${wrapIndex} fw-ad-${strokeIndex + 1}">${key.toUpperCase()}</p>`;
+        });
+        console.log("keysMarkup", keysMarkup);
+
+
+        let pointsMarkup = "";
+        pointsArr.forEach((pointsCount, strokeIndex) => {
+            pointsMarkup = pointsMarkup + `<p class="check-clone__text-${wrapIndex} fw-ad-${strokeIndex + 1}">${pointsCount}</p>`;
+        });
+
+
+        wrap.classList.add(`check-clone-${wrapIndex}`);
+        wrap.classList.add(`check-clone`);
+        wrap.innerHTML = `
+            <div>
+                ${keysMarkup}
+            </div>
+
+            <div class="check-clone__points">
+                ${pointsMarkup}
+            </div>`;
+        return wrap;
+
+    }
+    const checkElement = document.createElement("section");
+    checkElement.classList.add("check-screen");
+    for (let i = 0; i < 4; i++) {
+        checkElement.appendChild(getCheckLayer(checkData, i));
+    }
+    document.querySelector(parent).appendChild(checkElement);
+    const skyAndOcean = document.createElement("div");
+    skyAndOcean.classList.add("check-screen__sky-and-ocean");
+    skyAndOcean.innerHTML = getSkyAndOceanMarkup();
+    document.querySelector(parent).appendChild(skyAndOcean);
+}
+
+const getSkyAndOceanMarkup = () =>
+`
+<div id="sky" class="sky">
+    <div id="sun" class="sun"></div>
+    <div class="trees"></div>
+    <div class="trees-reverse"></div>
+    <div class="trees-bg"></div>
+</div>
+
+<div class="ocean">
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+    <div class="line-1"></div>
+</div>`;
+
+export { drawCheck, checkData };
