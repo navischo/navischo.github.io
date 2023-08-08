b19367a073fc2dd4a8ae96d71e315fdc1f8a4eb0
Егор <beast.loby@gmail.com>
2023-08-08 13:34:31 +0300

    add terminal

--- js/utils/initTerminal.js
+++ js/utils/initTerminal.js
@@ -1,11 +1,20 @@
 const STRINGS = [
     `“Cyberspace. A consensual hallucination experienced daily by billions of legitimate operators, in every nation, by children being taught mathematical concepts... A graphic representation of data abstracted from banks of every computer in the human system. Unthinkable complexity. Lines of light ranged in the nonspace of the mind, clusters and constellations of data. Like city lights, receding...” ― William Gibson, Neuromancer`,
-
+    `
+        Дідусь Рік вигадав як заробити на утримання свого секретного комплексу на Луні, не привертаючи багато уваги.<br><br>
+        
+        Ти влаштуєш Рейв... на Луні<br>
+        Хіба це не мрія? По всій Новій Цитаделі вже ширяться чутки що в C-137 готується дещо велике.<br>
+        Хто таке пропустить?<br><br>
+        
+        Залишилось тільки все підготувати, це як раз на тобі,<br>
+        Початок через годину
+    `
 ]
 
-const initTerminal = (nextString = STRINGS[0]) => {
+const initTerminal = (nextString = STRINGS[1]) => {
     const terminal = document.querySelector(".js-terminal");
-    terminal.textContent = nextString;
+    terminal.innerHTML = `${nextString}`;
 
     let element = document.querySelector('.screen');
     let string = element.innerText;
