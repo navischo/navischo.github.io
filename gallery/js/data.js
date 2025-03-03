988f28baa35ce528bba6b6bcf9d8d10f2a20a081
Егор <beast.loby@gmail.com>
2025-03-03 12:44:58 +0200

    upd

---
+++ gallery/js/data.js
@@ -0,0 +1,494 @@
+const COLLECTION_NAMES = {
+    ignorance: "Ignorance",
+    peacefulSleep: "Peaceful sleep",
+    milk: "Milk+",
+    neuromance: "Neuromance",
+    remix: "Remix",
+    replica: "Replica",
+    currents: "Currents"
+};
+// years 19, 20, 21, 22, 23, 24, 25
+const data = [
+    {
+        id: 1,
+        year: 24,
+        collection: COLLECTION_NAMES.currents
+    },
+    {
+        id: 2,
+        year: 24,
+        collection: COLLECTION_NAMES.currents
+    },
+    {
+        id: 3,
+        year: 24,
+        collection: COLLECTION_NAMES.currents
+    },
+    {
+        id: 4,
+        year: 23,
+        collection: COLLECTION_NAMES.currents
+    },
+    {
+        id: 5,
+        year: 21,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 6,
+        year: 21,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 7,
+        year: 21,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 8,
+        year: 21,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 9,
+        year: 21,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 10,
+        year: 21,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 11,
+        year: 20,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 12,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 13,
+        year: 21,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 14,
+        year: 20,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 15,
+        year: 20,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 16,
+        year: 21,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 17,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 18,
+        year: 20,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 19,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 20,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 21,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 22,
+        year: 21,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 23,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 24,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 25,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 26,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 27,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 28,
+        year: 21,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 29,
+        year: 21,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 30,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 31,
+        year: 21,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 32,
+        year: 21,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 33,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 34,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 35,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 36,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 37,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 38,
+        year: 21,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 39,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 40,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 41,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 42,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 43,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 44,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 45,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 46,
+        year: 22,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 47,
+        year: 22,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 48,
+        year: 22,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 49,
+        year: 22,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 50,
+        year: 22,
+        collection: COLLECTION_NAMES.milk
+    },
+    {
+        id: 51,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 52,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 53,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 54,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 55,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 56,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 57,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 58,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 59,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 60,
+        year: 21,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 61,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 62,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 63,
+        year: 20,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 64,
+        year: 21,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 65,
+        year: 22,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 66,
+        year: 22,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 67,
+        year: 21,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 68,
+        year: 21,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 69,
+        year: 21,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 70,
+        year: 21,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 71,
+        year: 21,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 72,
+        year: 21,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 73,
+        year: 22,
+        collection: COLLECTION_NAMES.peacefulSleep
+    },
+    {
+        id: 74,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 75,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 76,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 77,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 78,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 79,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 80,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 81,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 82,
+        year: 21,
+        collection: COLLECTION_NAMES.ignorance
+    },
+    {
+        id: 83,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 84,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 85,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 86,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 87,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 88,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 89,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 90,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 91,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 92,
+        year: 22,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 93,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 94,
+        year: 22,
+        collection: COLLECTION_NAMES.neuromance
+    },
+    {
+        id: 95,
+        year: 21,
+        collection: COLLECTION_NAMES.remix
+    },
+    {
+        id: 96,
+        year: 21,
+        collection: COLLECTION_NAMES.neuromance
+    }
+];
+
+export {COLLECTION_NAMES, data};
---
+++ gallery/js/dom.js
@@ -0,0 +1,46 @@
+import { COLLECTION_NAMES, data } from "./data.js";
+
+const init = () => {
+    const parent = document.querySelector(".swiper-wrapper");
+    const appendSheet = (imgId) => {
+        const newSlide = document.createElement("div");
+        const imgNode = document.createElement("img");
+        newSlide.classList.add("swiper-slide");
+        imgNode.src = `img/${imgId}.jpg`;
+        newSlide.appendChild(imgNode)
+        parent.appendChild(newSlide);
+
+    }
+
+    for (let i = 1; i <= 96; i++) {
+        appendSheet(i);
+    }
+
+    const swiper = new Swiper(".mySwiper", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
+        loop: true,
+    });
+    swiper.on('slideChange', () => {
+        updTitle();
+    });
+
+    const titleYearNode = document.querySelector(".year");
+    const titleCollectionNode = document.querySelector(".collection");
+    const updTitle = () => {
+        console.log(`hello, slide number ${swiper.activeIndex}`, data[swiper.activeIndex]);
+        titleYearNode.textContent = data[swiper.activeIndex].year;
+        titleCollectionNode.textContent = data[swiper.activeIndex].collection;
+    }
+}
+
+export { init };
